6bc2e28298dcd1b4ab28c725942838e5dac90555
shivrya <shivrajkhose4@gmail.com>
2025-05-26 14:45:29 -0700

    use of #/bin/bash in shell scripting

--- Basics.sh
+++ Basics.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# above line tells that this is script is a bash script even if file is not saved with .sh extension
 
 # this is our first bash script
 # this is how we can comment in the bash script
